4df0c29f2445c2e04325cb453173f6ea0ee6a76b
B.Nagaraju <vanaraveda@gmail.com>
2026-04-13 19:51:31 +0530

    fix save order bug

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const express = require("express");
 const Razorpay = require("razorpay");
 const cors = require("cors");
 const crypto = require("crypto");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 console.log("RAZORPAY_KEY_ID:", process.env.RAZORPAY_KEY_ID);
-console.log("RAZORPAY_KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET);
+console.log("RAZORPAY_KEY_ID loaded");
 app.get("/", (req, res) => {
   res.send("Vanara Backend Running 🚀");
 });
@@ -23,7 +32,6 @@ const razorpay = new Razorpay({
 // ✅ CREATE ORDER
 app.post("/create-order", async (req, res) => {
   const { amount } = req.body;
-
   const options = {
     amount: amount * 100,
     currency: "INR",
@@ -38,7 +46,41 @@ app.post("/create-order", async (req, res) => {
     res.status(500).send("Error creating order");
   }
 });
+app.post("/save-order", async (req, res) => {
+  console.log("BODY:", req.body);
 
+  const {
+    razorpay_order_id,
+    razorpay_payment_id,
+    razorpay_signature
+  } = req.body;
+
+  // 🔐 VERIFY AGAIN (SERVER SIDE)
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    .update(body.toString())
+    .digest("hex");
+
+  if (expectedSignature !== razorpay_signature) {
+    return res.status(400).json({ error: "Invalid payment" });
+  }
+
+  // ✅ SAVE ONLY IF VALID
+  try {
+    await db.collection("orders").add({
+      ...req.body,
+      status: "Confirmed",
+      createdAt: new Date()
+    });
+
+    res.json({ status: "saved" });
+
+  } catch (err) {
+    res.status(500).json({ error: "failed" });
+  }
+});
 // ✅ VERIFY PAYMENT
 app.post("/verify-payment", (req, res) => {
   const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
